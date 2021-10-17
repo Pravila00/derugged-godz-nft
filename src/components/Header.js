149b66b9b1d8f4e6a99446dab8c9c5c59b17b563
Pravila <xauro3@gmail.com>
2021-10-17 13:49:16 +0200

    Fixed color in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ export default function Header() {
             <div class="row">
                 <div class="col-3 p-3">
                     <div class="text-center">
-                        MagicalDeers
+                        <h1>Reindeer</h1>
                     </div>
                 </div>
                 <div class="col-9 p-3">
-                    <nav class="navbar navbar-expand-lg navbar-light">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                         <div class="container-fluid">
                           <a class="navbar-brand" href="#home"><h3>HOME</h3></a>
                           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,13 +18,13 @@ export default function Header() {
                           </button>
                           <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                              <li class="nav-item link-white">
+                              <li class="nav-item">
                                 <a class="nav-link" href="#roadmap"><h4>ROADMAP</h4></a>
                               </li>
-                              <li class="nav-item link-white">
+                              <li class="nav-item">
                                 <a class="nav-link" href="#team"><h4>TEAM</h4></a>
                               </li>
-                              <li class="nav-item link-white">
+                              <li class="nav-item">
                                 <a class="nav-link" href="#faq"><h4>FAQ</h4></a>
                               </li>
                             </ul>
