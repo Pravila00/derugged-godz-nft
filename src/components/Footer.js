9e26434f5824769a6cd467235a974d67532958e5
Pravila <xauro3@gmail.com>
2022-01-16 16:41:49 +0100

    Updated Roadmap and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ export default function Home() {
     <footer>
             <div class="row col text-center">
                 <p>
-                    &copy; 2021 MagicalDeers Powered by <a class="link-secondary" href="https://solana.com/">Solana</a>
+                    &copy; 2022 Derugged Godz Sanctuary powered by <a class="link-secondary" href="https://solana.com/">Solana</a>
                 </p>
                 <p>
-                    <a href="#">
+                    <a href="https://twitter.com/derruged_godz">
                         <img src={twitter} class="img-fluid" width="50" height="50"/></a>
-                    <a href="#">
+                    <a href="https://discord.gg/Pd8ynZEX">
                         <img src={discord} class="img-fluid" width="50" height="50"/></a>
                 </p>
             </div>
--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -22,10 +22,38 @@ export default function Roadmap() {
         <h1><b>ROADMAP</b></h1>
         <Timeline theme={customTheme}>
         <Events>
-          <TextEvent date="Stage 0" 
+          <TextEvent date="STAGE 0" 
             text="Get Discord and Twitter Running.
                   Start community growth and work on Art
-                  Make our way to take our sit on Solana Blockchain" 
+                  Make our way to take our sit on Solana Blockchain." 
+            />
+
+          <TextEvent date="STAGE 1" 
+            text="Airdrop and minting 999 Derugged Godz. Everyone submitting a solscan between
+            Jan 1st and Jan 4th with a purchase of a Gyaku will get their wallet whitelisted.
+            Whitelisted wallets will be able to mint for free." 
+            />
+
+          <TextEvent date="STAGE 2" 
+            text="Listing in secondary market. A percentage of royalties will be used to improve
+            the project." 
+            />
+
+          <TextEvent date="STAGE 3" 
+            text="DAO Sanctuary, being a holder of a God will make you parte of the Sanctuary.
+            Holding a Godz means that you can not be rugged anymore. If you get rugged, we will reimport
+            you the amount of Sol you've lost." 
+            />
+
+          <TextEvent date="STAGE 4" 
+            text="Once we reach a certain amount of funds. Sanctuary will be ready. A place where people who
+            got rugged in another project will have a place in our Discord. A percentage of royalties and mint
+            funds will be used to help rugged ones." 
+            />
+
+          <TextEvent date="STAGE 5" 
+            text="This stage will be a poll for the Community. If community decides they want a token, we will launch
+            a token, or staking. This sage will be Community choice." 
             />
 
           <ImageEvent
@@ -41,13 +69,6 @@ export default function Roadmap() {
               </UrlButton>
             </div>
           </ImageEvent>
-
-          <YouTubeEvent
-            date="6/18/19"
-            id="6UnRHtwHGSE"
-            name="General Tso's Chicken recipe"
-            text="... and YouTube videos!"
-          />
         </Events>
       </Timeline>
     </div>
